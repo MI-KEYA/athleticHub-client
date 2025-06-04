05700babbc39c8b1ac5f0bd43aa2e0b1febde570
MI-KEYA <moniakeya913@gmail.com>
2025-06-04 17:38:39 +0600

    added carousel slider under header in Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../Components/Header';
+import Hero from '../Components/Hero';
 
 const Home = () => {
     return (
-        <div>
-            <h1>hello world</h1>
+        <div className=' w-2/3 mx-auto mb-10'>
+            <h2 className="text-3xl font-bold text-center text-blue-950 my-10">
+                Unleash the Athlete in You
+            </h2>
+            <Hero />
         </div>
     );
 };
